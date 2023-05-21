633d4894d0ff63a624d72af3fdc984fd880e01ce
das-adithya <adithyadas77@gmail.com>
2023-05-21 16:45:16 +0530

    char Replaced

--- string/char_occurence.py
+++ string/char_occurence.py
@@ -1,2 +1,11 @@
 #program to get a string from a given string where all occurrences of its first char
 #have been changed to '$', except the first char itself
+
+
+def char_occurence(str):
+    str=input("Enter a string: ")
+    char=str[0]
+    new_str=char+str[1:].replace(char,'$')
+    return new_str
+    
+print(char_occurence(str))    
